94068e7d68065ddd5d3e8b31b7bacad17415095d
barisbulutdemir <barisbulutdemir@gmail.com>
2023-06-02 17:17:46 +0200

    sa
    sa

--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -9,13 +9,15 @@ const cartSlice = createSlice({
     initialState,
     reducers: {
         addToCart: (state, action) => {
-            const itemIndex = state.items.findIndex(item => item.id === action.payload.id);
+            const itemIndex = state.items.findIndex((item) => item.id === action.payload.id);
+
             if (itemIndex >= 0) {
                 state.items[itemIndex].quantity += 1;
             } else {
                 state.items.push({ ...action.payload, quantity: 1 });
             }
         },
+
         removeFromCart: (state, action) => {
             state.items = state.items.filter(item => item.id !== action.payload.id);
         },
